35886aa71a705de4fd50fd8d0bfcb82a0fa586d0
Mohammed Omar <omartechmohammed@gmail.com>
2024-02-15 15:35:13 -0500

    completed my lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
+const BaseAPI = "http://localhost:4000/questions"
+
 function App() {
   const [page, setPage] = useState("List");
+
+  // useEffect(() => fetch(BaseAPI)
+  //   .then(response => response.json())
+  //   // .then(console.log)
+  //   .then(setPage)
+  // , []);  
 
   return (
     <main>
